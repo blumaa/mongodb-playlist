fc506cd6a51f89bd2bf7aac23cd30c9b89ec1509
Aaron Blum <blumaa@gmail.com>
2020-02-06 11:27:23 +0100

    added mocha sample test

---
+++ test/demo_test.js
@@ -0,0 +1,12 @@
+const mocha = require("mocha");
+
+const assert = require('assert')
+
+//describe tests
+describe("some demo tests", function() {
+  //create tests
+
+  it("adds two numbers together", function() {
+    assert(2 + 2 === 5);
+  });
+});
