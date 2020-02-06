40955428602193243528236c4bd4f787d6f16fc6
Aaron Blum <blumaa@gmail.com>
2020-02-06 11:36:52 +0100

    added first test of saving to db

---
+++ test/saving_test.js
@@ -0,0 +1,21 @@
+const mocha = require("mocha");
+
+const assert = require('assert')
+const OfficeChar = require('../models/officechars')
+
+//describe tests
+describe("Saving Records", function() {
+  //create tests
+
+  it("Saves a record to the DB", function(done) {
+    const char = new OfficeChar({
+      name: 'Jim'
+    });
+    char.save().then(function(){
+      assert(char.isNew === false);
+      done();
+    })
+  });
+
+  //next test
+});
